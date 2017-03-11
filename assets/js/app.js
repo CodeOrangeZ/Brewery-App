f36ed16a0b90234968708e7d026302285a5f26a5
Greg Lee <gml2136@gmail.com>
2017-03-10 22:15:08 -0800

    working on the brewery api call. running into a CORS issue that i'm not sure about, asking a TA tomorrow

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,66 +5,66 @@
 
 var movieUrl = "http://www.omdbapi.com/?";//API key not necessary
 var movieObj = {};
-var breweryUrl = "http://api.brewerydb.com/v2/"; //this is the base, the API endpoint will need to be specified
+var breweryUrl = "http://api.brewerydb.com/v2/beers/?"; //this is the base, the API endpoint will need to be specified
 var breweryAPIKey = "a2bbeb0349946cb230fb7cc9a584a5a4";
 const beerToRating = {
   "American Style Premium Lager" : {
-        "beerStyleId": 97,
-        "rating_min": 8.6,
-        "rating_max": 10.0
+        beerStyleId: 97,
+        rating_min: 8.6,
+        rating_max: 10.0
       },
 
   "South German Style Hefeweizen" : {
-        "beerStyleId": 48,
-        "rating_min": 8.1,
-        "rating_max": 8.5
+        beerStyleId: 48,
+        rating_min: 8.1,
+        rating_max: 8.5
       },
 
   "Belgian Style White" : {
-        "beerStyleId": 65,
-        "rating_min": 8.0,
-        "rating_max": 7.6
+        beerStyleId: 65,
+        rating_min: 8.0,
+        rating_max: 7.6
       },
 
   "American Style Lager" : {
-        "beerStyleId": 93,
-        "rating_min": 7.1,
-        "rating_max": 7.5
+        beerStyleId: 93,
+        rating_min: 7.1,
+        rating_max: 7.5
       },
   "Irish Style Red Ale" : {
-        "beerStyleId": 22,
-        "rating_min": 6.6,
-        "rating_max": 7.0
+        beerStyleId: 22,
+        rating_min: 6.6,
+        rating_max: 7.0
       },
 
   "American Style Pale Ale" : {
-        "beerStyleId": 25,
-        "rating_min": 6.1,
-        "rating_max": 6.5
+        beerStyleId: 25,
+        rating_min: 6.1,
+        rating_max: 6.5
       },
 
   "American Style IPA" : {
-        "beerStyleId": 30,
-        "rating_min": 5.6,
-        "rating_max": 6.0
+        beerStyleId: 30,
+        rating_min: 5.6,
+        rating_max: 6.0
       },
 
   "American Style Malt Liquor" : {
-        "beerStyleId": 100,
-        "rating_min": 5.1,
-        "rating_max": 5.5
+        beerStyleId: 100,
+        rating_min: 5.1,
+        rating_max: 5.5
       },
 
   "Imperial or Double IPA" : {
-        "beerStyleId": 31,
-        "rating_min": 4.1,
-        "rating_max": 5.0
+        beerStyleId: 31,
+        rating_min: 4.1,
+        rating_max: 5.0
       },
 
   "American Style Imperial Stout" : {
-        "beerStyleId": 43,
-        "rating_min": 0,
-        "rating_max": 4.0
+        beerStyleId: 43,
+        rating_min: 0,
+        rating_max: 4.0
       },
 
 };
@@ -83,33 +83,63 @@ const beerToRating = {
 
 
 
-$.ajax({url: queryURLBase, method:"GET"})
-.done(function(response) {
+// $.ajax({url: movieUrl, method:"GET"})
+// .done(function(response) {
+//
+// //call movie detail function
+//
+// //stored desired paramters in vars
+// 	movieObj.title = response.title;
+// 	movieObj.poster = response.poster;
+// 	movieObj.plot = response.plot;
+// 	movieObj.rating = response.rating;
+// 	console.log(response);
+// });
 
-//call movie detail function 
-
-//stored desired paramters in vars
-	movieObj.title = response.title;
-	movieObj.poster = response.poster;
-	movieObj.plot = response.plot;
-	movieObj.rating = response.rating;
-	console.log(response);
-});
-
-//create another function that will make ajax call 
+//create another function that will make ajax call
 // to beer and pass those vars ars arguments
 
 
 
 //response from OMDB will need to be operated on to map it to a specific beer style.
 //heavier beer for worse movies. Better movies, Lighter beer.
-
-
- 
-
-
+/**
+  * Function for extracting the appropriate beer based on ratings
+  * @param "beerMap" object - The object we use for holding the values
+  * @param "rate" number - the rating value we recieve from the OMBD api
+  */
+let extractStyle = (beerMap, rate) => {
+  for(key in beerMap) {
+    if(beerMap[key].rating_min <= rate <= beerMap[key].rating_max) {
+      return beerMap[key];
+    }
+  };
+}
+var beerObj = extractStyle(beerToRating, 4.5);
+var queryStyleId = beerObj.beerStyleId;
+var beerSearchParams = {
+  key: breweryAPIKey,
+  order: "random",
+  styleId: queryStyleId,
+  hasLabels: "Y"
+};
 
 //send request to breweryDB for specific beer style.
+breweryUrl+=$.param(beerSearchParams);
+let brewJax = (breweryUrl) => {
+  $.ajax({
+    url:breweryUrl,
+    method: "GET",
+    xhrFields: {
+      withCredentials: true
+   }
+  })
+  .done(function(res){
+    console.log(res);
+  })
+}
+
+
 
 
 
