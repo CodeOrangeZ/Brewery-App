208e35c584d1a0f0b9b66acc31fdbe8ab16c2a54
Iman <imanhamidi91@gmail.com>
2017-03-14 21:33:45 -0700

    Omg thank you Brandon.

--- assets/js/JSforBEER.js
+++ assets/js/JSforBEER.js
@@ -1,89 +1,89 @@
-$(document).ready(function() {
+// $(document).ready(function() {
 
 
-  $(".contentDiv").addClass("hide");
+//   $(".contentDiv").addClass("hide");
 
 
-  // instanciate new modal 'are you 21?'
-  var modal = new tingle.modal({
-      footer: true,
-      stickyFooter: false,
-      closeLabel: "Close",
-      cssClass: ['custom-class-1', 'custom-class-2'],
-      onOpen: function() {
-          console.log('modal open');
-      },
-      onClose: function() {
-          console.log('modal closed');
-      },
-      beforeClose: function() {
-          // here's goes some logic
-          // e.g. save content before closing the modal
-          return true; // close the modal
-        return false; // nothing happens
-      }
-  });
+//   // instanciate new modal 'are you 21?'
+//   var modal = new tingle.modal({
+//       footer: true,
+//       stickyFooter: false,
+//       closeLabel: "Close",
+//       cssClass: ['custom-class-1', 'custom-class-2'],
+//       onOpen: function() {
+//           console.log('modal open');
+//       },
+//       onClose: function() {
+//           console.log('modal closed');
+//       },
+//       beforeClose: function() {
+//           // here's goes some logic
+//           // e.g. save content before closing the modal
+//           return true; // close the modal
+//         return false; // nothing happens
+//       }
+//   });
 
-  // set content
-  modal.setContent('<h1>Are you 21 years of age or older?</h1>');
+//   // set content
+//   modal.setContent('<h1>Are you 21 years of age or older?</h1>');
 
-  // add a button
-  modal.addFooterBtn('Yes', 'yesBtn tingle-btn tingle-btn--primary', function() {
-      // here goes some logic
-      modal.close();
-      $(".contentDiv").removeClass("hide");
+//   // add a button
+//   modal.addFooterBtn('Yes', 'yesBtn tingle-btn tingle-btn--primary', function() {
+//       // here goes some logic
+//       modal.close();
+//       $(".contentDiv").removeClass("hide");
 
-      var audio = document.getElementsByTagName("audio")[0];
-      audio.play();
-
-
-
-        var fill=document.querySelector(".water-fill");
-        TweenMax.fromTo(fill,0.8,{
-          attr:{
-            x:-400
-          }
-        },
-        {
-          attr:{
-            x:0,
-          },
-
-          repeat:-1,
-          ease:Linear.easeNone
-        });
-
-        TweenMax.fromTo(fill,10,{
-          attr:{
-            y:120,
-            height:0
-          },
-        },{
-          attr:{
-            y:7,
-            height:140
-          },
-          repeat: 0,
-          yoyo:true,
-          ease:Linear.easeNone
-        });
+//       var audio = document.getElementsByTagName("audio")[0];
+//       audio.play();
 
 
 
-  });
+//         var fill=document.querySelector(".water-fill");
+//         TweenMax.fromTo(fill,0.8,{
+//           attr:{
+//             x:-400
+//           }
+//         },
+//         {
+//           attr:{
+//             x:0,
+//           },
 
-  // add another button
-  modal.addFooterBtn('No', 'tingle-btn tingle-btn--danger', function() {
-      // here goes some logic
-      modal.close();
-      $(".not21").removeClass('hide');
+//           repeat:-1,
+//           ease:Linear.easeNone
+//         });
+
+//         TweenMax.fromTo(fill,10,{
+//           attr:{
+//             y:120,
+//             height:0
+//           },
+//         },{
+//           attr:{
+//             y:7,
+//             height:140
+//           },
+//           repeat: 0,
+//           yoyo:true,
+//           ease:Linear.easeNone
+//         });
+
+
+
+//   });
+
+//   // add another button
+//   modal.addFooterBtn('No', 'tingle-btn tingle-btn--danger', function() {
+//       // here goes some logic
+//       modal.close();
+//       $(".not21").removeClass('hide');
       
-  });
+//   });
 
-  // open modal
-  modal.open();
+//   // open modal
+//   modal.open();
 
-});
+// });
 
 
 
